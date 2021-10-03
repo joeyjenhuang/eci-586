114aabd869d2fb51cb4096d4943acc494d892b6d
Shaun Kellogg <sbkellog@ncsu.edu>
2021-10-03 16:20:24 +0000

    clean data

--- unit-2/noodle.R
+++ unit-2/noodle.R
@@ -5,23 +5,38 @@ library(tidyverse)
 
 sci_mo_with_text <- read_csv("data/sci-mo-with-text.csv")
 
-set.seed(123)
 
 course_data <- 
-  sci_mo_with_text %>% 
-  clean_names() %>%
-  mutate(
-    at_risk = ifelse(final_grade >= 70, "late", "on_time"),
-    # We will use the date (not date-time) in the recipe below
-    date = lubridate::as_date(time_hour)
-  ) %>% 
-  # Include the weather data
-  inner_join(weather, by = c("origin", "time_hour")) %>% 
-  # Only retain the specific columns we will use
-  select(dep_time, flight, origin, dest, air_time, distance, 
-         carrier, date, arr_delay, time_hour) %>% 
-  # Exclude missing data
+  sci_mo_with_text |>
+  clean_names() |>
+  mutate(pass_fail = ifelse(final_grade >= 60, "pass", "on_time")) |>
+  select(student_id,
+         int,
+         uv,
+         pc,
+         time_spent,
+         final_grade,
+         subject,
+         enrollment_reason,
+         semester,
+         enrollment_status,
+         cogproc,
+         social,
+         posemo,
+         negemo,
+         n) %>%   
   na.omit() %>% 
-  # For creating models, it is better to have qualitative columns
-  # encoded as factors (instead of character strings)
   mutate_if(is.character, as.factor)
+
+course_data
+
+course_data %>% 
+  count(arr_delay) %>% 
+  mutate(prop = n/sum(n))
+
+ #split data
+
+set.seed(123)
+
+
+
